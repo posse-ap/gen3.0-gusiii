6455a1821176fb11223a0d85f703911602d30077
gusiii <zhenyoudachuan615@gmail.com>
2022-10-25 01:11:25 +0900

    モバイル画面整えました

--- web.app/scripts/loading.js
+++ web.app/scripts/loading.js
@@ -1,11 +1,19 @@
 'use strict'
 {
   const loading_start = document.getElementById('submit');
+  const sp_loading_start = document.getElementById('sp_submit');
   const loading = document.getElementById('loading');
   const mask = document.getElementById('mask');
   const completion = document.getElementById('completion');
   const close = document.getElementById('close');
   loading_start.addEventListener('click', () =>{
+    loading.classList.remove('hidden');
+    completion.classList.remove('hidden');
+    window.setTimeout(() => {
+      loading.classList.add('hidden');
+  }, 3000);
+  });
+  sp_loading_start.addEventListener('click', () =>{
     loading.classList.remove('hidden');
     completion.classList.remove('hidden');
     window.setTimeout(() => {
--- web.app/scripts/modal.js
+++ web.app/scripts/modal.js
@@ -2,13 +2,20 @@
 
 
   const open = document.getElementById("open");
+  const spopen = document.getElementById("sp_open");
   const submit = document.getElementById("submit");
+  // const spsubmit = document.getElementById("submit");
   const loading = document.getElementById('loading');
   const modal = document.getElementById("modal");
   const mask = document.getElementById("mask");
   const modalclose = document.getElementById("modal_close");
 
   open.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    mask.classList.remove("hidden");
+  });
+
+  spopen.addEventListener("click", () => {
     modal.classList.remove("hidden");
     mask.classList.remove("hidden");
   });
@@ -31,7 +38,7 @@
 {
   const studyDate = document.getElementById("js-studydate");
   studyDate.innerHTML =
-    '<input type="text" name="today" id="calendarTEST" class="study_date_box">';
+    '<input type="text" name="today" id="calendarTEST" value="YYYY-MM-DD" class="study_date_box">';
   flatpickr.localize(flatpickr.l10ns.ja);
   flatpickr('#calendarTEST');
 }
@@ -63,27 +70,34 @@
 {
   const studyTime = document.getElementById("js-studytime");
   studyTime.innerHTML =
-    '<input type="number" name="time" min="0" max="24" class="study_time_box" required>';
+    '<input type="number" name="time" min="1" max="24" class="study_time_box" required>';
 }
 {
   const studyComment = document.getElementById("js-studycomment");
   studyComment.innerHTML =
-    '<input type="text" name="text" minlength="1" maxlength="140" class="study_comment_box" required>';
+    '<textarea name="text" minlength="1" maxlength="140" class="study_comment_box" id="js_tweet" rows=”3″ cols=”50″ wrap=”hard” required></textarea>';
 }
 {
   const submit = document.getElementById("submit");
+  const spsubmit = document.getElementById("sp_submit");
   const studyTwitter = document.getElementById("js-studytwitter");
   studyTwitter.innerHTML = `<input type="checkbox" name="share" id="twitter_share"><label for="twitter_share" required>Twitterにシェアする</label>`;
 
   const chk3 = document.getElementById("twitter_share");
-  chk3.addEventListener("click", () => {
-    if (chk3.checked) {
+  
+  
+  chk3.addEventListener("change", function(){
+    if (this.checked) {
       console.log("twitterにシェアします");
       chk3.classList.add("tweet");
-      const tweetUrl = "https://twitter.com/intent/tweet";
+      const tweetUrl = "https://twitter.com/intent/tweet?text="+ encodeURIComponent(document.getElementById("js_tweet").value)
+      null;
       submit.addEventListener("click", () => {
       window.location.href = tweetUrl;
-      
+      // window.open = tweetUrl;
+    });
+      spsubmit.addEventListener("click", () => {
+      window.location.href = tweetUrl;
     });
   }
   });
